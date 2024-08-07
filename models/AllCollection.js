a926499f2e4d6007d85e8ebf38ead8e8678e54ad
DESKTOP-3RPTQE6\pc <kcherumkottilmu0349@conestogac.on.ca>
2024-08-06 20:09:20 -0400

    push the changes

---
+++ models/AllCollection.js
@@ -0,0 +1,156 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: Number,
+        unique: true
+    },
+    first: {
+        type: String,
+    },
+    last: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+const counterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    value: {
+        type: Number,
+        default: 0
+    }
+});
+
+const ingredientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: Map,
+        of: [String]
+    },
+    ingredients: [{
+        type: String,
+        required: true
+    }],
+    instructions: [{
+        type: String,
+        required: true
+    }],
+    nutrition: {
+        calories: {
+            type: Number,
+            required: true
+        },
+        protein: {
+            type: String,
+            required: true
+        },
+        carbohydrates: {
+            type: String,
+            required: true
+        },
+        fat: {
+            type: String,
+            required: true
+        }
+    }
+});
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    image: { type: String, required: false },
+    location: { type: String, required: false },
+    userId: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+  });
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const blogSchema = new mongoose.Schema({
+    userId: {
+        type: Number,
+        required: true
+    },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    comments: [
+      {
+        user: { type: String, required: true },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  });
+  
+const Blog = mongoose.model('Blogs', blogSchema);
+const Post = mongoose.model('Posts', postSchema);
+const Product = mongoose.model('Products', productSchema);
+const Recipe = mongoose.model('Recipe', recipeSchema);
+const Ingredient = mongoose.model('Ingredient', ingredientSchema);
+const User = mongoose.model('User', userSchema);
+const Counter = mongoose.model('Counter', counterSchema);
+
+module.exports = {
+    User,
+    Ingredient,
+    Recipe,
+    Product,
+    Post,  
+    Blog ,
+    Counter
+};
---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const authRoutes = require('./routes/authRoutes');
+const reciepeRoutes = require('./routes/reciepeRoutes');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  transports: ['websocket', 'polling'],
+  pingInterval: 25000,
+  pingTimeout: 60000,
+});
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(cors());
+
+app.use('/api', [authRoutes, reciepeRoutes]);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('newBlog', (blog) => {
+    io.emit('newBlog', blog);
+  });
+
+  socket.on('updateBlog', (updatedBlog) => {
+    io.emit('updateBlog', updatedBlog);
+  });
+
+  socket.on('deleteBlog', (blogId) => {
+    io.emit('deleteBlog', blogId);
+  });
+
+  socket.on('newComment', (comment) => {
+    io.emit('newComment', comment);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const mongoURI = process.env.MONGO_URI ;
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+  const port = process.env. add || 3001;
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}).catch((error) => {
+  console.error('Error connecting to MongoDB', error);
+});
